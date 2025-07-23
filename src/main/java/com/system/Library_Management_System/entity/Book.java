3e07583d450f83ca692068255337099f97c8e287
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-07-23 22:50:32 +0530

    add enum booktype

--- src/main/java/com/system/Library_Management_System/entity/Book.java
+++ src/main/java/com/system/Library_Management_System/entity/Book.java
@@ -24,8 +24,9 @@ public class Book {
     @Column(nullable = false)
     private String author;
 
+    @Column(nullable = false)
     private String publisher;
-
+    
     private String genre;
 
     @Enumerated(EnumType.STRING)
