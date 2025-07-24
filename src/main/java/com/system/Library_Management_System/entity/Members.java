0617ec46521eb05dc6bdcf60bc743c63143a5886
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-07-25 01:28:19 +0530

    adding  member entity and membershiptype enums

--- src/main/java/com/system/Library_Management_System/entity/Members.java
+++ src/main/java/com/system/Library_Management_System/entity/Members.java
@@ -1,7 +1,45 @@
 package com.system.Library_Management_System.entity;
 
+import com.system.Library_Management_System.enums.MembershipType;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = " memeber")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Members {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,length = 10)
+    private String PhoneNumber; 
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private MembershipType membershipType = MembershipType.BASIC;
+
+    private double totalFines;
+
+    
+
+
+
+
+
+
+
     
 }
---
+++ src/main/java/com/system/Library_Management_System/enums/MembershipType.java
@@ -0,0 +1,7 @@
+package com.system.Library_Management_System.enums;
+
+public enum MembershipType {
+    BASIC,
+    SILVER,
+    PREMIUM
+}
